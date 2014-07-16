8749ab52c304ab980cbf66abf7caeae3993d0f82
Ravin <ravin@gocardless.com>
2014-07-16 21:25:46 +0100

    create datafile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716164800) do
+ActiveRecord::Schema.define(version: 20140716202302) do
+
+  create_table "data_files", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
