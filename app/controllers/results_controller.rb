fa1cb02caa50890b7658cb940ea47743b9fe8fbb
Ravin <ravin@gocardless.com>
2014-08-05 23:21:31 +0100

    DO NOT MERGE arbitrary number of features support in analysis but not in the view

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -25,22 +25,26 @@ class ResultsController < ApplicationController
 			file = File.open(path,'rb')
 			long_version = file.read
 			file_split_by_line = long_version.split "\r"
-			puts file_split_by_line.class
 			independents = []
 			dependents = []
-			count = 0
-			file_split_by_line.each do |kl|
-			  fl = kl.split(",")
-			  fl.each do |fkl|
-			    count +=1
-			    if (count%2) > 0
-			      independents << fkl.to_i
+			file_split_by_line.each do |training_example|
+				example_dependents = []
+				parameter_number = 0
+			  list_of_parameters = training_example.split(",")
+			  list_of_parameters.each do |parameter|
+			    parameter_number +=1
+			    if parameter_number == 1
+			      independents << parameter.to_i
 			    else
-			      dependents << fkl.to_i
+			      example_dependents << parameter.to_i
 			    end
 			  end
+			  dependents << example_dependents
 			end
-
+			puts "and the dependents are "
+			puts dependents
+			puts "and the dependents length is "
+			puts dependents.length
 			return [independents,dependents]
 		end
 
@@ -50,10 +54,32 @@ class ResultsController < ApplicationController
 			independent_parameter_name = independents.shift
 			dependent_parameter_name = dependents.shift
 			ones = Array.new(independents.length, 1)
-			theta_vector = Matrix.row_vector(ones)
-			theta_matrix = Matrix.rows(theta_vector.to_a << dependents)
+			theta_matrix = Matrix.row_vector(ones)
+			theta_matrix_as_array = []
+			puts "dependents length is #{dependents.length}"
+			(0..dependents[0].length-1).each do |parameter_index|
+				param_vec = []
+				dependents.each do |dependent_paramater_vector|
+					param_vec << dependent_paramater_vector[parameter_index]
+				end
+				theta_matrix_as_array << param_vec
+				puts 'param vev is'
+				puts param_vec
+			end
+			puts "theta matrix as array is #{theta_matrix_as_array.length}"
+			puts theta_matrix_as_array
+			theta_matrix_as_array.each do |parameter_array|
+				theta_matrix = Matrix.rows(theta_matrix.to_a << parameter_array)
+			end
 			theta_matrix = theta_matrix.transpose
 			y_vector = Matrix.row_vector(independents).transpose
+			puts 'theta_matrix is'
+			theta_matrix
+			puts 'y vector is'
+			puts y_vector
 			result_matrix = (((theta_matrix.transpose * theta_matrix).inverse)*theta_matrix.transpose*y_vector)
+			puts 'result matrix is'
+			puts result_matrix
+			result_matrix
 		end
 end
