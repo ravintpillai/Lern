4da6dcf2f28e2446c02fd5d6590332f1aa4501be
Ravin <ravin@gocardless.com>
2014-08-09 00:54:47 +0100

    cleaning up the results controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -45,10 +45,6 @@ class ResultsController < ApplicationController
 			  end
 			  dependents << example_dependents
 			end
-			puts "and the dependents are "
-			puts dependents
-			puts "and the dependents length is "
-			puts dependents.length
 			return [independents,dependents]
 		end
 
@@ -60,30 +56,18 @@ class ResultsController < ApplicationController
 			ones = Array.new(independents.length, 1)
 			theta_matrix = Matrix.row_vector(ones)
 			theta_matrix_as_array = []
-			puts "dependents length is #{dependents.length}"
 			(0..dependents[0].length-1).each do |parameter_index|
 				param_vec = []
 				dependents.each do |dependent_paramater_vector|
 					param_vec << dependent_paramater_vector[parameter_index]
 				end
 				theta_matrix_as_array << param_vec
-				puts 'param vev is'
-				puts param_vec
 			end
-			puts "theta matrix as array is #{theta_matrix_as_array.length}"
-			puts theta_matrix_as_array
 			theta_matrix_as_array.each do |parameter_array|
 				theta_matrix = Matrix.rows(theta_matrix.to_a << parameter_array)
 			end
 			theta_matrix = theta_matrix.transpose
 			y_vector = Matrix.row_vector(independents).transpose
-			puts 'theta_matrix is'
-			theta_matrix
-			puts 'y vector is'
-			puts y_vector
 			result_matrix = (((theta_matrix.transpose * theta_matrix).inverse)*theta_matrix.transpose*y_vector)
-			puts 'result matrix is'
-			puts result_matrix
-			result_matrix
 		end
 end
