bc01680e2d004239c097745d0a9a23523fc01c26
Ravin <ravin@gocardless.com>
2014-08-09 16:29:52 +0100

    adding devise secret key for precompilation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,7 +230,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
